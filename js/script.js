55f8be01cbae5eb2ddef0af24c402ccb116421a0
mohamed kamal <mohamed.kamal999@yahoo.com>
2019-09-13 00:36:32 +0200

    web

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#sidebarToggle").on('click', function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+      });
+    
+      $(document).on('scroll', function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+
+      $('.scroll-to-top').click(function(){
+        $('body,html').animate({
+            scrollTop : 0
+        },500);
+    });
+})
